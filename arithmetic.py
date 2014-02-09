631147822d00c794ba8c9737060f83f9da8904de
Mitzi Song <mitzisong@mitzi-songs-macbook.local>
2014-02-09 11:37:17 -0800

    Finished exercise

---
+++ arithmetic.py
@@ -0,0 +1,23 @@
+def add(first, second):
+    return first + second
+
+def subtract(first, second):
+    return first - second
+
+def multiply(first, second):
+    return first * second
+
+def divide(first, second):
+    return first / second
+
+def square(i):
+    return i**2
+
+def cube(i):
+    return i**3
+
+def mod(i, j):
+    return i%j
+
+def power(i, j):
+    return i**j
